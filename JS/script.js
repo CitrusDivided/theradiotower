a9d401c8ba03d21bb9f5da322e88beff7556669d
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2022-09-27 13:20:30 -0700

    Restructuring core layout in attempt at redesigning

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,27 @@
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y < 800) {
-    $(".div3, .div4, .div5").fadeIn();
-    } else {
-        $(".div3, .div4, .div5").fadeOut();
-    }
-});
+// $(document).scroll(function() {
+//     var y = $(this).scrollTop();
+//     if (y < 800) {
+//     $(".div3, .div4, .div5").fadeIn();
+//     } else {
+//         $(".div3, .div4, .div5").fadeOut();
+//     }
+// });
 
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 800){
-        $('.div8').css({
-            "height" : "400%",
-        });
-        $('.div9').css({
-            "height" : "400%",
-        })
-    } else {
-        $(".div8").css({
-            "height" : "5%"
-        });
-        $('.div9').css({
-            "height" : "30%",
-        })
-    }
-})
+// $(document).scroll(function() {
+//     var y = $(this).scrollTop();
+//     if (y > 800){
+//         $('.div8').css({
+//             "height" : "400%",
+//         });
+//         $('.div9').css({
+//             "height" : "400%",
+//         })
+//     } else {
+//         $(".div8").css({
+//             "height" : "5%"
+//         });
+//         $('.div9').css({
+//             "height" : "30%",
+//         })
+//     }
+// })
